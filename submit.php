9bd4cf08c45b39ef3a8321e54863104fbf37fac5
j-ishii <ishii@j-ishiis-MacBook-Air.local>
2013-07-26 22:03:23 +0900

    just this side of beta ver.
    Add movie file upload function for not-chrome users and iOS6 users,
    deny webRTC users.
    Avoid filename conflict of same UNIX timestamp files.

--- submit.php
+++ submit.php
@@ -5,58 +5,90 @@ $dateandtime = date("Y-m-d H:i:s");
 $user_agent = $_SERVER['HTTP_USER_AGENT'];
 $timezone =  date_default_timezone_get();
 
-echo "アップロードファイル名　：　" , $_FILES["video"]["name"] , "<BR>";
-echo "MIMEタイプ　：　" , $_FILES["video"]["type"] , "<BR>";
-$size = $_FILES["video"]["size"];
-echo "ファイルサイズ　：　" , $size , "<BR>";
-$tmpfile = $_FILES["video"]["tmp_name"];
-echo "テンポラリファイル名　：　" , $tmpfile , "<BR>";
-echo "エラーコード　：　" , $_FILES["video"]["error"] , "<BR>";
-echo "送信者名　：" , $_POST["fullname"] , "<BR>";
-echo "メッセージ内容　： " , $_POST["message"] , "<BR>";
+$flag = $_POST['flag'];
+if($flag){
+	$size = $_FILES["video"]["size"];
+	$tmpfile = $_FILES["video"]["tmp_name"];
+	$filename = $_FILES["video"]["name"] .'.webm';
+	$savefolder = 'movies';
 
-$filename = $_FILES["video"]["name"] .'.webm';
-$savefolder = 'movies';
-
-if(isset($size)){
-	$result = move_uploaded_file($tmpfile, './' . $savefolder . '/' . $filename);
-	if($result == true){
-		echo "upload success";
-		$sentence = '"' . $_POST["fullname"] . '",';
-		$sentence .= '"' . $_POST["message"] . '",';
-		$sentence .= '"' . $filename . '",';
-		$sentence .= '"' . $savefolder . '",';
-		$sentence .= '"' . $ipAddress . '",';
-		$sentence .= '"' . $dateandtime . '",';
-		$sentence .= '"' . $user_agent . '",';
-		$sentence .= '"' . $timezone . '",';
-		$sentence .= '"' . '1' . '"' . "\n";
-		$fp = fopen("databasefile.csv","a");
-		fwrite($fp,$sentence);
-		$fclose($fp);
+	if(isset($size)){
+		$result = move_uploaded_file($tmpfile, './' . $savefolder . '/' . $filename);
+		if($result == true){
+			echo "upload success";
+			$sentence = '"' . $_POST["fullname"] . '",';
+			$sentence .= '"' . $_POST["message"] . '",';
+			$sentence .= '"' . $filename . '",';
+			$sentence .= '"' . $savefolder . '",';
+			$sentence .= '"' . $ipAddress . '",';
+			$sentence .= '"' . $dateandtime . '",';
+			$sentence .= '"' . $user_agent . '",';
+			$sentence .= '"' . $timezone . '",';
+			$sentence .= '"' . '1' . '"' . "\n";
+			$fp = fopen("databasefile.csv","a");
+			fwrite($fp,$sentence);
+			fclose($fp);
+			echo "upload success!!";
+		}else{
+			echo "upload failed";
+		}
 	}else{
-		echo "upload failed";
+		echo "no file uploaded error!";
 	}
 }else{
-	echo "no file uploaded error!";
+	$type = $_FILES["video"]["type"];
+	$size = $_FILES["video"]["size"];
+	$tmpfile = $_FILES["video"]["tmp_name"];
+
+	$extention = null;
+
+	if($type === "video/webm"){
+		$extention = ".webm";
+	}else if($type === "video/mepg"){
+		$extention = ".mepg";
+	}else if($type === "video/mp4"){
+		$extention = ".mp4";
+	}else if($type === "video/ogg"){
+		$extention = ".ogv";
+	}else if($type === "video/quicktime"){
+		$extention = ".mov";
+	}else if($type === "video/x-msvideo"){
+		$extention = ".avi";
+	}
+
+	if(isset($size)){
+
+		$filename = $_FILES["video"]["name"] .$extention;
+		$savefolder = 'notRTCmovies';
+
+		if(!is_null($extention)){
+			$result = move_uploaded_file($tmpfile, './' . $savefolder . '/' . $filename);
+			if($result == true){
+				$sentence = '"' . $_POST["fullname"] . '",';
+				$sentence .= '"' . $_POST["message"] . '",';
+				$sentence .= '"' . $filename . '",';
+				$sentence .= '"' . $savefolder . '",';
+				$sentence .= '"' . $ipAddress . '",';
+				$sentence .= '"' . $dateandtime . '",';
+				$sentence .= '"' . $user_agent . '",';
+				$sentence .= '"' . $timezone . '",';
+				$sentence .= '"' . '1' . '"' . "\n";
+				$fp = fopen("databasefile.csv","a");
+				fwrite($fp,$sentence);
+				fclose($fp);
+				echo "upload success!!";
+			}else{
+				echo "upload failed";
+			}
+		}else{
+			echo "file type is unsupported error!";
+		}
+	}else{
+		echo "no file uploaded error!";
+	}
+
 }
 
 //header('Access-Control-Allow-Origin:*');
 
 ?>
-
-
-<!DOCTYPE html>
-<html lang="jp">
-<head>
-	<meta charset="utf-8">
-	<title>movie submit page</title>	
-
-
-</head>  
-  
-<body>
-
-</body>
-
-</html>
